b50d55bfdf57e1e878d9130303965695b5f4e811
Hoang Quan <nguyen.quan.it.99@gmail.com>
2020-06-21 10:33:09 +0700

    commit lan 11

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log("FE42");
 
 console.log("FE42 mot lan nua :)");
 console.log("FE42 lan nua ne ");
-console.log("Hello Cybersoft!!!");
+console.log("Hello Cybersoft456!!!");
 
 console.log("Thu tao branch moi!!!");
